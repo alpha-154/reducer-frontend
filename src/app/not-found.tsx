ead02bb7bf92414c90ca9e49ebbaf22e89da11db
alpha-154 <mrhrakib07@gmail.com>
2024-11-30 18:25:27 +0600

    fixed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className='min-h-screen flex flex-col gap-5 items-center justify-center'>
-      <h1 className='text-3xl text-white'>Page <span className='text-red-500'>Not Found</span></h1>
-      <p className='text-2xl text-white'>Sorry, we couldn&apos;t find the page you&apos;re looking for.</p>
-      <Link href="/" className='text-xl text-white'>Go back home</Link>
+      <h1 className='text-3xl text-darkbrownText'>Page <span className='text-red-500'>Not Found</span></h1>
+      <p className='text-2xl text-darkbrownText text-center'>Sorry, we couldn&apos;t find the page you&apos;re looking for.</p>
+      <Link href="/" className='text-xl text-darkbrownText'>Go back home</Link>
     </div>
   );
 }
